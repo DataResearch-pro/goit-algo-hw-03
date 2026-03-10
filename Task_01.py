f9580af646071fa6fc1ab71f52f61e85602a53f0
Paul <letter.workbook@gmail.com>
2026-03-10 12:05:05 +0200

    Editing function arguments

--- Task_01.py
+++ Task_01.py
@@ -4,27 +4,24 @@ from datetime import datetime
 #⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶
 
 
-def get_days_from_today():
+def get_days_from_today(date: str) -> int:
     '''Calculating the difference between today's date and any other date (in days)'''
-    # Вхідні дані та операційні змінні
-    date_custom: str
+    # Операційні змінні
     date_convert: datetime
     date_difference: int
 
     # Основний блок
     try:
-        date_custom     = input("Enter the date in the format YYYY-MM-DD: ")        # Отримання дати від користувача
-        date_convert    = datetime.strptime(date_custom, '%Y-%m-%d')                # Конвертація рядка у дату
-        date_difference = (datetime.today() - date_convert).days * (-1)             # Різниця дат (у днях)
+        date_convert    = datetime.strptime(date, '%Y-%m-%d')                # Конвертація рядка у дату
+        date_difference = (datetime.today() - date_convert).days * (-1)      # Різниця дат (у днях)
     except ValueError:
-        print(f"Date '{date_custom}' is incorrect. For example, 2026-01-29.")       # Стандартний виняток
-    else:
-        print(f"\nDifference: {date_difference} (days)\n")                          
-        #return date_difference                                                     # Якщо потрібен експорт результату
+        print(f"Date '{date}' is incorrect. For example, 2026-01-29.")       # Стандартний виняток
+    else:                         
+        return date_difference
 
 
 #⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶⊷⊶
 
 
 if __name__=='__main__':
-    get_days_from_today()
+    print(get_days_from_today('2026-01-01'))
